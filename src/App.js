72dde8f6abc231f19512350d486f5f720168a806
Polina Zolotukhina <polina@iwoork.com>
2018-11-13 17:27:59 +0000

    renderProps

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import CloneElement from './CloneElement';
 import Context from './Context';
 import Hoc from './ButtonOne';
 import Candy from './Candy';
+import ToggleRPC from './renderProps/ToggleRPC';
+import ToggleRenderProps from './renderProps/ToggleRenderProps'
 
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -17,6 +20,24 @@ class App extends Component {
         <Hoc />
         <Hoc  error/>
         <Candy />
+        //render
+        <ToggleRenderProps render={({name, on, toggle})=>(
+            <div>
+                <h1>{name}</h1>
+                { on&&<h1>Show me!</h1>}
+                <button onClick={toggle}> Show/Hide</button>
+            </div>
+        )} / >
+        //children
+        <ToggleRPC>
+         {({name, on, toggle})=>(
+            <div>
+                <h1>{name}</h1>
+                { on&&<h1>Show me!</h1>}
+                <button onClick={toggle}> Show/Hide</button>
+            </div>
+        )}
+        </ToggleRPC>
       </div>
     );
   }
